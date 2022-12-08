6e2a40e674a23da67ef824c1bc1564b9a6e1c80b
崔世文 <cuisw@gzsendi.cn>
2022-12-08 11:40:06 +0800

    加入测温模块，并且添加双屏异显

---
+++ app/src/main/java/com/sendi/deliveredrobot/BaseFragment.java
@@ -0,0 +1,241 @@
+package com.sendi.deliveredrobot;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Presentation;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.hardware.display.DisplayManager;
+import android.media.MediaRouter;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class BaseActivity extends Fragment {
+    public Presentation mPresentation;
+    public Display mDisplay;
+
+    public MediaRouter mMediaRouter;
+    public DisplayManager mDisplayManager;
+
+    public FrameLayout frameLayout;
+    //    private Button bt_presentation;
+    Context context;
+
+    public int flag = 0;    //witch method used to show presentation 0.none 1. mediarouter 2.displaymanager
+
+    private static final String TAG = "BaseAcyivity";
+
+
+
+    //onResume和onPause一般用来进行对presentsatoin中的内容进行额外的处理
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Listen for changes to media routes.
+        mMediaRouter.addCallback(MediaRouter.ROUTE_TYPE_LIVE_VIDEO, mMediaRouterCallback);
+        // Register to receive events from the display manager.
+        mDisplayManager.registerDisplayListener(mDisplayListener, null);
+        Show(flag);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Listen for changes to media routes.
+        mMediaRouter.removeCallback(mMediaRouterCallback);
+        mDisplayManager.unregisterDisplayListener(mDisplayListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Dismiss the presentation when the activity is not visible.
+        if (mPresentation != null) {
+            Log.i(TAG, "Dismissing presentation because the activity is no longer visible.");
+            mPresentation.dismiss();
+            mPresentation = null;
+            //  flag=0;
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMediaRouter = (MediaRouter) getActivity().getSystemService(Context.MEDIA_ROUTER_SERVICE);
+        mDisplayManager = (DisplayManager) getActivity().getSystemService(Context.DISPLAY_SERVICE);
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
+    }
+
+    private final class MyPresentation extends Presentation {
+
+
+        public MyPresentation(Context context, Display display) {
+            super(context, display);
+        }
+
+//        public MyPresentation(Context context, Display display,
+//                              DemoPresentationContents contents) {
+//            super(context, display);
+//            mContents = contents;
+//        }
+
+        /**
+         * Sets the preferred display mode id for the presentation.
+         */
+//        public void setPreferredDisplayMode(int modeId) {
+//            //       mContents.displayModeId = modeId;
+//
+//            WindowManager.LayoutParams params = getWindow().getAttributes();
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                params.preferredDisplayModeId = modeId;
+//            }
+//            getWindow().setAttributes(params);
+//        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            // Be sure to call the super class.
+            super.onCreate(savedInstanceState);
+
+            // Get the resources for the context of the presentation.
+            // Notice that we are getting the resources from the context of the presentation.
+            Resources r = getContext().getResources();
+            // Inflate the layout.
+            setContentView(R.layout.presentation_content);
+            frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+//            getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+//                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
+//            bt_presentation = (Button) findViewById(R.id.bt_presentation);
+//            bt_presentation.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    AlertDialog dialog = new AlertDialog.Builder(context)
+//                            .setTitle("Dialog")
+//                            .setMessage("Prsentation Click Test")
+//                            .setPositiveButton("OK", new OnClickListener() {
+//                                @Override
+//                                public void onClick(DialogInterface dialog, int which) {
+//
+//                                }
+//                            }).create();
+//                    dialog.show();
+//                }
+//            });
+
+        }
+    }
+
+    public final DisplayManager.DisplayListener mDisplayListener =
+            new DisplayManager.DisplayListener() {
+                @Override
+                public void onDisplayAdded(int displayId) {
+                    Log.d(TAG, "Display #" + displayId + " added.");
+                    Show(flag);
+                }
+
+                @Override
+                public void onDisplayChanged(int displayId) {
+                    Log.d(TAG, "Display #" + displayId + " changed.");
+                    Show(flag);
+                }
+
+                @Override
+                public void onDisplayRemoved(int displayId) {
+                    Log.d(TAG, "Display #" + displayId + " removed.");
+                    Show(flag);
+                }
+            };
+
+    public void Show(int flag) {
+        switch (flag) {
+            case 1:
+                ShowPresentationByMediarouter();
+                break;
+            case 2:
+                ShowPresentationByDisplaymanager();
+                break;
+        }
+    }
+
+    public void ShowPresentationByDisplaymanager() {
+        Display[] presentationDisplays = mDisplayManager.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION);
+        if (presentationDisplays.length > 0) {
+            // If there is more than one suitable presentation display, then we could consider
+            // giving the user a choice.  For this example, we simply choose the first display
+            // which is the one the system recommends as the preferred presentation display.
+            Display display = presentationDisplays[0];
+            showPresentation(display);
+        }
+    }
+
+    public void ShowPresentationByMediarouter() {
+        MediaRouter.RouteInfo route = mMediaRouter.getSelectedRoute(MediaRouter.ROUTE_TYPE_LIVE_VIDEO);//选择类型
+        if (route != null) {
+            mDisplay = route.getPresentationDisplay();
+            showPresentation(mDisplay);
+        }
+    }
+
+    public void showPresentation(Display presentationDisplay) {
+        // Dismiss the current presentation if the display has changed.
+        if (mPresentation != null && mPresentation.getDisplay() != presentationDisplay) {
+            Log.i(TAG, "Dismissing presentation because the current route no longer "
+                    + "has a presentation display.");
+            mPresentation.dismiss();
+            mPresentation = null;
+        }
+        // Show a new presentation if needed.
+        if (mPresentation == null && presentationDisplay != null) {
+            Log.i(TAG, "Showing presentation on display: " + presentationDisplay);
+            mPresentation = new MyPresentation(getContext(), presentationDisplay);
+            //  mPresentation.setOnDismissListener(mOnDismissListener);
+            try {
+                mPresentation.show();
+            } catch (WindowManager.InvalidDisplayException ex) {
+                Log.w(TAG, "Couldn't show presentation!  Display was removed in "
+                        + "the meantime.", ex);
+                mPresentation = null;
+            }
+        }
+    }
+    //MediaRouter检测HDMI线的拔出和插入用的。
+    public final MediaRouter.SimpleCallback mMediaRouterCallback =
+            new MediaRouter.SimpleCallback() {
+                @Override
+                public void onRouteSelected(MediaRouter router, int type, MediaRouter.RouteInfo info) {
+                    Log.d(TAG, "onRouteSelected: type=" + type + ", info=" + info);
+                    Show(flag);
+                }
+
+                @Override
+                public void onRouteUnselected(MediaRouter router, int type, MediaRouter.RouteInfo info) {
+                    Log.d(TAG, "onRouteUnselected: type=" + type + ", info=" + info);
+                    Show(flag);
+                }
+
+                @Override
+                public void onRoutePresentationDisplayChanged(MediaRouter router, MediaRouter.RouteInfo info) {
+                    Log.d(TAG, "onRoutePresentationDisplayChanged: info=" + info);
+                    Show(flag);
+                }
+            };
+
+    //DisplayManager检测HDMI线的拔出和插入用的。
+
+
+}
+
+
