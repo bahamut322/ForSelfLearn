558a63a00d4edede8a03f07886e4b8501730a514
heky <heky@gzsendi.cn>
2024-04-08 18:10:16 +0800

    增加问答页面loading时间以保证初始化完成

--- app/src/main/java/com/sendi/deliveredrobot/view/fragment/ConversationFragment.kt
+++ app/src/main/java/com/sendi/deliveredrobot/view/fragment/ConversationFragment.kt
@@ -354,6 +354,7 @@ class ConversationFragment : Fragment() {
                 }
                 initSDK()
                 startRecord()
+                Thread.sleep(1000)
                 DialogHelper.loadingDialog.dismiss()
             }
             SpeakHelper.speakUserCallback = object : SpeakHelper.SpeakUserCallback {
