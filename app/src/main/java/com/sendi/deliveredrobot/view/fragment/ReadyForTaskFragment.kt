95d1edd024aad88fd12bd47152ba45f8607c17df
heky <heky@gzsendi.cn>
2024-04-22 14:33:35 +0800

    机器人充电对接失败，重新对接充电桩的时候播报修改为“重新对接充电桩”

--- app/src/main/java/com/sendi/deliveredrobot/view/fragment/ReadyForTaskFragment.kt
+++ app/src/main/java/com/sendi/deliveredrobot/view/fragment/ReadyForTaskFragment.kt
@@ -13,12 +13,15 @@ import com.sendi.deliveredrobot.R
 import com.sendi.deliveredrobot.databinding.FragmentReadyForTaskBinding
 import com.sendi.deliveredrobot.entity.Universal
 import com.sendi.deliveredrobot.helpers.SpeakHelper
+import com.sendi.deliveredrobot.navigationtask.BillManager
+import com.sendi.deliveredrobot.navigationtask.GoBackTaskBill
 
 /**
  * @describe 准备工作中页面
  */
 class ReadyForTaskFragment : Fragment() {
     lateinit var binding:FragmentReadyForTaskBinding
+    private var speakText = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,14 +32,19 @@ class ReadyForTaskFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        SpeakHelper.speakWithoutStop(getString(R.string.i_ready_to_work_excuse_me))
+        speakText = if (BillManager.currentBill() is GoBackTaskBill) {
+            getString(R.string.retry_dock)
+        }else{
+            getString(R.string.i_ready_to_work_excuse_me)
+        }
+        SpeakHelper.speakWithoutStop(speakText)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
         binding.bottomAlarmTextViewReadyForTask.apply {
-            bottomAlarmText1 = getString(R.string.i_ready_to_work_excuse_me)
+            bottomAlarmText1 = speakText
         }
         binding.motionLayoutReadyForTask.apply {
             transitionToState(R.id.state2)
